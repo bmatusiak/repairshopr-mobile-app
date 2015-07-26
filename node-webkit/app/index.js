69376754119fb9c43d085b3cc35127ca470ee4c3
Bradley Matusiak <usabrad86@bellsouth.net>
2015-07-26 21:07:15 +0000

    examples for where files can com from

--- node-webkit/app/index.js
+++ node-webkit/app/index.js
@@ -1,1 +1,11 @@
+
+//  Local Files
 window.location = "../../index.html";
+
+//  Github Files
+//window.location = "https://github-raw-relay.herokuapp.com/bmatusiak/repairshopr-mobile-app/master/index.html";
+
+//  c9.io Files
+//window.location = "https://myapp-username.c9.io/";
+
+
