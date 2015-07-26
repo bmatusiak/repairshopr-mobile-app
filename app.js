7969852751d325ba44d88e321c795b61877c6039
Bradley Matusiak <usabrad86@bellsouth.net>
2015-07-26 22:40:24 +0000

    update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 (function() {
 /* globals requirejs */
   
-  var appPath = "https://github-raw-relay.herokuapp.com/bmatusiak/repairshopr-mobile-app/master";
+  //var appPath = "https://github-raw-relay.herokuapp.com/bmatusiak/repairshopr-mobile-app/master";
+  var appPath = ".";
 
   requirejs.config({
     waitSeconds: 200,
