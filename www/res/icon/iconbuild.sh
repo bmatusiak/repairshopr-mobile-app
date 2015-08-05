2efa94c426c36a4eb012c5d159f197af5dd1e043
Bradley Matusiak <usabrad86@bellsouth.net>
2015-08-05 22:39:28 +0000

    updated icons

---
+++ www/res/icon/iconbuild.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+convert -resize 62x62 icon-196.png windows-phone/icon-62-tile.png
+convert -resize 48x48 icon-196.png windows-phone/icon-48.png
+convert -resize 173x173 icon-196.png windows-phone/icon-173-tile.png
+convert -resize 57x57 icon-196.png ios/icon-57.png
+convert -resize 113x113 icon-196.png ios/icon-57-2x.png
+convert -resize 72x72 icon-196.png ios/icon-72.png
+convert -resize 144x144 icon-196.png ios/icon-72-2x.png
+convert -resize 128x128 icon-196.png bada/icon-128.png
+convert -resize 64x64 icon-196.png webos/icon-64.png
+convert -resize 80x80 icon-196.png blackberry/icon-80.png
+convert -resize 128x128 icon-196.png tizen/icon-128.png
+convert -resize 48x48 icon-196.png android/icon-48-mdpi.png
+convert -resize 36x36 icon-196.png android/icon-36-ldpi.png
+convert -resize 72x72 icon-196.png android/icon-72-hdpi.png
+convert -resize 96x96 icon-196.png android/icon-96-xhdpi.png
+convert -resize 50x50 icon-196.png bada-wac/icon-50-type3.png
+convert -resize 80x80 icon-196.png bada-wac/icon-80-type4.png
+convert -resize 48x48 icon-196.png bada-wac/icon-48-type5.png
